b04d5efd91651fa6970dcc26228857af3a92b617
Isaac <BenBagBag@users.noreply.github.com>
2024-06-06 11:12:08 -0700

    remove creds

--- 1-Authentication/0-sign-in-vanillajs/App/public/authConfig.js
+++ 1-Authentication/0-sign-in-vanillajs/App/public/authConfig.js
@@ -5,11 +5,9 @@
  */
 const msalConfig = {
     auth: {
-        // clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-        // authority: 'https://Enter_the_Tenant_Subdomain_Here.ciamlogin.com/', // Replace the placeholder with your tenant subdomain        
+        clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
+        authority: 'https://Enter_the_Tenant_Subdomain_Here.ciamlogin.com/', // Replace the placeholder with your tenant subdomain        
         redirectUri: 'http://localhost:3000/redirect', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
-        clientId: '94fc755a-af32-43f9-9ce6-5f86f2beb928', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://TrialTenantkwFwHYij.ciamlogin.com/', // Replace the placeholder with your tenant subdomain        redirectUri: 'http://localhost:3000/redirect', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
         navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
     },
     cache: {
