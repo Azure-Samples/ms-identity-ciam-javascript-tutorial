451dd6fc04a70cb0cd4b5c00491664ceb7493e5f
Deji Adeoti <jadeoti@microsoft.com>
2025-01-21 12:11:16 +0300

    For the redirect scenario, override the redirectUri in the global configuration. Otherwise, redirection will land on the empty redirect page.

--- 1-Authentication/0-sign-in-vanillajs/App/public/authRedirect.js
+++ 1-Authentication/0-sign-in-vanillajs/App/public/authRedirect.js
@@ -81,7 +81,7 @@ function signIn() {
      * You can pass a custom request object below. This will override the initial configuration. For more information, visit:
      * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/request-response-object.md#request
      */
-
+    loginRequest.redirectUri = "/";
     myMSALObj.loginRedirect(loginRequest);
 }
 
